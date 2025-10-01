fa7cf1476482a9c7519783b8380de1b6f3aeb76b
pedro-vin <pedro.vinicius1310@gmail.com>
2025-09-30 21:20:42 -0300

    Criação da Página Inicial e do Cabeçalho

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+// import About from './pages/About';
+// import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header /> {/* Cabeçalho visível em todas as páginas */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/sobre" element={<About />} /> */}
+        {/* <Route path="/contato" element={<Contact />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+// src/pages/Home.jsx
+function Home() {
+  return (
+    <div>
+      <h1>Bem-vindo à página inicial!</h1>
+      <p>Esta é a tela Home do seu site React.</p>
+    </div>
+  );
+}
+
+export default Home;
