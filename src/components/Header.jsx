4d8e62ae1a56336aa37b7910788e9101929dde54
pedro-vin <pedro.vinicius1310@gmail.com>
2025-10-02 23:11:08 -0300

    Adição de novos botões na Sidebar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 // src/components/Header.jsx
 import { Link } from 'react-router-dom';
 import './Header.css';
-import { ReactComponent as PanelIcon } from "../assets/icons/PanelIcon.svg";
-import { ReactComponent as NotificationsIcon } from "../assets/icons/NotificationsIcon.svg";
-import { ReactComponent as HelpIcon } from "../assets/icons/HelpIcon.svg";
-import { ReactComponent as SettingsIcon } from "../assets/icons/SettingsIcon.svg";
-import { ReactComponent as ProfileIcon } from "../assets/icons/ProfileIcon.svg";
+import { ReactComponent as PanelIcon } from "../assets/icons/Panel.svg";
+import { ReactComponent as NotificationsIcon } from "../assets/icons/Notifications.svg";
+import { ReactComponent as HelpIcon } from "../assets/icons/Help.svg";
+import { ReactComponent as SettingsIcon } from "../assets/icons/Settings.svg";
+import { ReactComponent as ProfileIcon } from "../assets/icons/Profile.svg";
 
 function Header({ toggleSidebar }) {
   return (
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,18 @@
 // src/components/Sidebar.jsx
 import { Link } from "react-router-dom";
-import { ReactComponent as UserIcon } from "../assets/icons/UserIcon.svg";
+import { ReactComponent as UserIcon } from "../assets/icons/User.svg";
 import { ReactComponent as CreateEventIcon } from "../assets/icons/CreateEvent.svg";
 import { ReactComponent as AvailableEventsIcon } from "../assets/icons/AvailableEvents.svg";
+import { ReactComponent as DoubtsIcon } from "../assets/icons/Doubts.svg";
+import { ReactComponent as FiltersIcon } from "../assets/icons/Filters.svg";
+import { ReactComponent as CertificateIcon } from "../assets/icons/Certificate.svg";
+import { ReactComponent as FeedbackIcon } from "../assets/icons/Feedback.svg";
 import "./Sidebar.css";
 
 export default function Sidebar({ isOpen }) {
   return (
     <aside className={`sidebar ${isOpen ? "open" : "closed"}`}>
       <ul>
-
         <li>
           <Link to="/">
             <UserIcon width={20} height={20} />
@@ -23,15 +26,43 @@ export default function Sidebar({ isOpen }) {
             <span>Criar Evento</span>
           </Link>
         </li>
-        
+
         <li>
           <Link to="/">
             <AvailableEventsIcon width={20} height={20} />
             <span>Eventos Disponíveis</span>
           </Link>
         </li>
-        
+
+        <li>
+          <Link to="/">
+            <DoubtsIcon width={20} height={20} />
+            <span>Dúvidas</span>
+          </Link>
+        </li>
+
+        <li>
+          <Link to="/">
+            <FiltersIcon width={20} height={20} />
+            <span>Filtros</span>
+          </Link>
+        </li>
+
+        <li>
+          <Link to="/">
+            <CertificateIcon width={20} height={20} />
+            <span>Certificados</span>
+          </Link>
+        </li>
       </ul>
+
+      <div className="sidebar-footer">
+        <div className="footer-divider"></div>
+        <Link to="/logout" className="sidebar-footer-link">
+          <FeedbackIcon width={16} height={16} />
+          <span>Ajuda e Feedback</span>
+        </Link>
+      </div>
     </aside>
   );
 }
