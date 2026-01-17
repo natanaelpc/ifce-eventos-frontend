8fcd4ad94e294692e64ad72fb4a447eb868c7618
izaacpinheiro <izaacpinheiro.123@gmail.com>
2026-01-16 23:51:55 -0300

    feat: integra registro com backend

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,54 +1,105 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import "./Auth.css";
 import LogoIf from '../assets/logoif.png';
+import { useUser } from "../context/UserContext";
+import api from "../services/APIService";
+import { useState } from 'react';
 
 function Register() {
+
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [nome, setNome] = useState("");
+  const [erro, setErro] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setErro("");
+
+    if (!email || !senha) {
+      setErro("Preencha e-mail, senha e nome.");
+      return;
+    }
+
+    if (senha !== confirmPassword) {
+      setErro("As senhas não coincidem.");
+      return;
+    }
+
+    try {
+      const res = await api.post("/auth/register", {
+        "email": email,
+        "senha": senha,
+        "nome": nome
+      });
+
+      navigate("/login");
+
+    } catch (err) {
+        if (err.response) {
+          setErro("E-mail, senha ou nome inválidos.");
+      } else {
+          setErro("Erro ao conectar o servidor");
+      }
+    }
+  };
+
   return (
     <div className="auth-container register">
       <img src={LogoIf} alt="LogoIf" className="ifce-logo" />
       <div className="auth-card">
         <h2 className="auth-title">Criar uma conta</h2>
 
-        <form className="auth-form">
+        <form className="auth-form" onSubmit={handleSubmit}>
           <div className="form-group">
             <label htmlFor="name">Nome</label>
             <input 
               id="name"
               type="text" 
-              placeholder="Digite seu nome" 
+              placeholder="Digite seu nome"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
               required 
             />
           </div>
 
           <div className="form-group">
-            <label htmlFor="email">Email</label>
+            <label htmlFor="email">E-mail</label>
             <input 
               id="email"
               type="email" 
-              placeholder="Digite seu email" 
+              placeholder="Digite seu e-mail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               required 
             />
           </div>
 
           <div className="form-group">
             <label htmlFor="password">Senha</label>
-            <input 
-              id="password"
-              type="password" 
-              placeholder="Digite sua senha" 
-              required 
+            <input
+              id="senha"
+              type="password"
+              placeholder="Digite sua senha"
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
+              required
             />
           </div>
 
           <div className="form-group">
-            <label htmlFor="confirmPassword">Confirmar senha</label>
-            <input 
-              id="confirmPassword"
-              type="password" 
-              placeholder="Confirme sua senha" 
-              required 
-            />
-          </div>
+          <label htmlFor="confirmPassword">Confirmar senha</label>
+          <input
+            id="confirmPassword"
+            type="password"
+            placeholder="Confirme sua senha"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+        </div>
 
           <button type="submit" className="btn-primary">
             Criar Conta
@@ -58,11 +109,12 @@ function Register() {
             Continuar com o Google
           </button>
         </form>
+        
       </div>
 
-        <footer className="login-footer">
-          <p>Já tem uma conta? <Link to="/">Entre agora</Link></p>
-        </footer>
+      <footer className="login-footer">
+        <p>Já tem uma conta? <Link to="/login">Entre agora</Link></p>
+      </footer>
 
     </div>
   );
