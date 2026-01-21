c2a5d3353dbdb1c025e965d2e605dd7d8d457d9b
izaacpinheiro <izaacpinheiro.123@gmail.com>
2026-01-20 21:54:28 -0300

    feat: adiciona sistema de logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,52 @@
-// src/components/Header.jsx
-import { Link } from 'react-router-dom';
-import './Header.css';
+
+import { Link, useNavigate } from "react-router-dom";
+import "./Header.css";
 import { ReactComponent as PanelIcon } from "../assets/icons/Panel.svg";
-//import { ReactComponent as NotificationsIcon } from "../assets/icons/Notifications.svg";
-//import { ReactComponent as HelpIcon } from "../assets/icons/Help.svg";
-//import { ReactComponent as SettingsIcon } from "../assets/icons/Settings.svg";
 import { ReactComponent as ProfileIcon } from "../assets/icons/Profile.svg";
+import { ReactComponent as LogoutIcon } from "../assets/icons/Logout.svg";
+import { useUser } from "../context/UserContext";
 
 function Header({ toggleSidebar }) {
+  const { logout } = useUser();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    const confirmLogout = window.confirm(
+      "Tem certeza que deseja sair?"
+    );
+
+    if (confirmLogout) {
+      logout();
+      navigate("/login");
+    }
+  };
+
   return (
     <header className="header">
-      {/* Botão para abrir/fechar sidebar */}
+      {/* Botão abrir/fechar sidebar */}
       <button className="sidebar-toggle-icon" onClick={toggleSidebar}>
         <PanelIcon width={20} height={20} />
       </button>
 
       <nav>
         <ul className="nav-list">
-          <li><Link to="/profile"><ProfileIcon width={20} height={20} /></Link></li>
+          <li>
+            <Link to="/profile">
+              <ProfileIcon width={20} height={20} />
+            </Link>
+          </li>
+
+          {/* botão loggout (somente ícone) */}
+          <li>
+            <button
+              type="button"
+              className="header-icon-button logout-icon"
+              onClick={handleLogout}
+              title="Sair"
+            >
+              <LogoutIcon width={20} height={20} />
+            </button>
+          </li>
         </ul>
       </nav>
     </header>
